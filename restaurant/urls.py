7ac3609066d4df27d2ddfa15d115cd229db3bfc0
Oleksandr <mrjelice@gmail.com>
2023-05-26 21:36:34 -0500

    API

---
+++ restaurant/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import MenuItemsView,SingleMenuItemView
+
+urlpatterns = [
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+]
